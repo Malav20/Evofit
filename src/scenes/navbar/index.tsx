24f033a1b2303f27f4620a4ebb19fb13ee0b7e3b
Malav Parekh <sunnyparekh8@gmail.com>
2023-04-08 23:52:11 +0530

    Fixed styling issue

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -5,6 +5,7 @@ import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
+
 type Props = {
   isTopOfPage: boolean;
   selectedPage: SelectedPage;
@@ -16,6 +17,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
+
   return (
     <nav>
       <div
@@ -23,8 +25,10 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
       >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
-            {/* Left Side */}
+            {/* LEFT SIDE */}
             <img alt="logo" src={Logo} />
+
+            {/* RIGHT SIDE */}
             {isAboveMediumScreens ? (
               <div className={`${flexBetween} w-full`}>
                 <div className={`${flexBetween} gap-8 text-sm`}>
@@ -67,16 +71,18 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
-      {/* Mobile Menu Modal */}
+
+      {/* MOBILE MENU MODAL */}
       {!isAboveMediumScreens && isMenuToggled && (
-        <div className="z-60 fixed bottom-0 right-0 h-full w-[300px] bg-primary-100 drop-shadow-xl">
-          {/* Close Icon */}
+        <div className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* CLOSE ICON */}
           <div className="flex justify-end p-12">
             <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-              <XMarkIcon className="h-6 w-6 text-gray-400"></XMarkIcon>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
             </button>
           </div>
-          {/* Menu Items */}
+
+          {/* MENU ITEMS */}
           <div className="ml-[33%] flex flex-col gap-10 text-2xl">
             <Link
               page="Home"
